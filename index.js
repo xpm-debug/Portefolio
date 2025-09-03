75206af9c95affdd7d5761f0ca4504f76e70f830
Xavier <xsionnet@gmail.com>
2025-09-03 21:32:59 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
 // index.js
 // Ajoutez ici votre code JavaScript personnalisé
 console.log("Portfolio chargé !");
+
+document.getElementById("backBtn").addEventListener("click", () => {
+    window.history.back();
+});
+
