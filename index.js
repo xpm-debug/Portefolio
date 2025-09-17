dbb0a0332b8133b8d511598ee1f16bd54458a821
Xavier <xsionnet@gmail.com>
2025-09-17 18:39:21 +0200

    Update index.js

--- index.js
+++ index.js
@@ -24,3 +24,18 @@ document.getElementById('showPart2').addEventListener('click', function() {
     document.getElementById('part2').style.display = "flex";
 });
 
+
+// Gestion des boutons Partie 1 et Partie 2
+document.getElementById('showPart1').addEventListener('click', function() {
+    document.getElementById('part1').style.display = "flex";
+    document.getElementById('part2').style.display = "none";
+    document.getElementById('showPart1').classList.add('active');
+    document.getElementById('showPart2').classList.remove('active');
+});
+
+document.getElementById('showPart2').addEventListener('click', function() {
+    document.getElementById('part1').style.display = "none";
+    document.getElementById('part2').style.display = "flex";
+    document.getElementById('showPart1').classList.remove('active');
+    document.getElementById('showPart2').classList.add('active');
+});
